e8cdd5c3d3e3ae021bffa29e56eacd055e863c18
ryukftw <workforbijoy9@gmail.com>
2025-02-16 07:06:00 +0000

    RMX3371: Build OplusDolby

--- device.mk
+++ device.mk
@@ -142,6 +142,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
 # Dolby Manager
+PRODUCT_PACKAGES += \
+    OplusDolby
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml
 
